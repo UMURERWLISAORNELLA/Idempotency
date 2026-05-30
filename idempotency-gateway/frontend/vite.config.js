8ac6a929b62be297d02f27aa06811e6fef6abbc0
UMURERWLISAORNELLA <umurerwalisaornella@gmail.com>
2026-05-30 18:28:05 +0200

    Updated

--- idempotency-gateway/frontend/vite.config.js
+++ idempotency-gateway/frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: 'all',
+    host: '0.0.0.0',
+    port: 4173,
+  },
   server: {
     port: 5173,
     proxy: {
